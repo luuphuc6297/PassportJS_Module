768cbb8a9a4debe9eb1eb932d09ce6b2088515a3
luuphuc <luuphuc6297@gmail.com>
2019-05-19 16:19:30 +0700

    Big Update

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {ensureAuthenticated} = require('../config/auth');
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-router.get('/', (req, res) => res.render('welcome'));
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
-router.get('/dashboard',ensureAuthenticated ,(req, res) => res.render('dashboard'), {
-    name: req.user.name
-});
-
-router.use('/user', require('../routes/user'));
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+    res.render('dashboard', {
+        user: req.user
+    })
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,93 +1,95 @@
 const express = require('express');
 const router = express.Router();
-const User  = require('../models/user');
 const bcrypt = require('bcryptjs');
-const errors = require("mongoose");
 const passport = require('passport');
+// Load User model
+const User = require('../models/User');
+const { forwardAuthenticated } = require('../config/auth');
 
-//Login Page
-router.get('/login', (req, res, next) =>{
-    res.render('login')
-});
+// Login Page
+router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 
-//Register Page
-router.get('/register', (req, res, next) =>{
-    res.render('register')
-});
+// Register Page
+router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
 
-router.post('/register', (req, res) =>{
-   const{name, email, password ,password2} = req.body;
-   let errors = [];
-    if(!name || !email || !password || !password2 ){
-      errors.push({msg: "Please fill all fields"})
+// Register
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please enter all fields' });
     }
-    if(password !== password2){
-        errors.push({msg: "Password do no match"})
+
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
     }
-    if(password.length < 6){
-        errors.push({msg: "Password should be at least 6 characters"})
+
+    if (password.length < 6) {
+        errors.push({ msg: 'Password must be at least 6 characters' });
     }
-    if(errors.length > 0){
-       res.render('register', {
+
+    if (errors.length > 0) {
+        res.render('register', {
             errors,
             name,
             email,
             password,
             password2
-       });
-    }
-    else {
-        User.findOne({email: email})
-            .then(user => {
-               if (user) {
-                errors.push({msg: "Email is already register"});
-                    res.render('register', {
-                        errors,
-                        name,
-                        email,
-                        password,
-                        password2
-                    });
-               }
-               else
-               {
-                const newUser = new User({
+        });
+    } else {
+        User.findOne({ email: email }).then(user => {
+            if (user) {
+                errors.push({ msg: 'Email already exists' });
+                res.render('register', {
+                    errors,
                     name,
                     email,
                     password,
                     password2
                 });
-                //Hash password
-                   bcrypt.genSalt(10, (err, salt) => {
-                       bcrypt.hash(newUser.password, salt, (err, hash) => {
-                           if (err) throw err;
-                           newUser.password = hash;
-                           newUser
-                               .save()
-                               .then(user => {
-                                   req.flash('success_msg', 'You are now registered and can log in');
-                                   res.redirect('/user/login');
-                               })
-                               .catch(err => console.log(err));
-                       });
-                   });
-               }
-            });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => {
+                                req.flash(
+                                    'success_msg',
+                                    'You are now registered and can log in'
+                                );
+                                res.redirect('/users/login');
+                            })
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
     }
 });
-//Login Handle
-router.post('/login', (req, res, next) =>{
-    passport.authenticate('local',{
-        successRedirect: "/dashboard",
-        failureRedirect: "/user/login",
+
+// Login
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
         failureFlash: true
     })(req, res, next);
 });
 
-//Log out Handle
-router.get('/logout', (req, res, next)=>{
+// Logout
+router.get('/logout', (req, res) => {
     req.logout();
-    req.flash('success_mgs', 'You are logged out');
-    res.redirect('/user/login');
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
 });
+
 module.exports = router;
